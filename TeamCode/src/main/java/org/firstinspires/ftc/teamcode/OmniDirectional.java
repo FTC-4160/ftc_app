f2ca5f76d85c4edf6144187544ae37b101fc6018
KingofGamesYami <steven.dirth@gmail.com>
2016-10-12 17:57:57 -0500

    Update MultiDirectional OpMode to support Robot and telemetry messages.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDirectional.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDirectional.java
@@ -10,28 +10,28 @@ import com.qualcomm.robotcore.util.Range;
  */
 @TeleOp( name = "OmniDirectional" )
 public class OmniDirectional extends OpMode {
-    private DcMotor fl, fr, bl, br;
 
     @Override
     public void loop(){
         double turn = gamepad1.left_trigger - gamepad1.right_trigger;
-        double rightFront = Range.clip( -gamepad1.right_stick_y - gamepad1.left_stick_x + turn, -1, 1 );
-        double leftFront = Range.clip( gamepad1.right_stick_y - gamepad1.left_stick_x + turn, -1, 1 );
-        double rightBack = Range.clip( gamepad1.right_stick_y + gamepad1.left_stick_x + turn, -1, 1 );
-        double leftBack = Range.clip( -gamepad1.right_stick_y + gamepad2.left_stick_x + turn, -1, 1 );
+        double drivey = -gamepad1.right_stick_y;
+        double drivex = -gamepad1.right_stick_x;
+        double rightFront = Range.clip( drivey  - drivex + turn, -1, 1 );
+        double leftFront =  Range.clip( -drivey - drivex + turn, -1, 1 );
+        double rightBack =  Range.clip( -drivey + drivex + turn, -1, 1 );
+        double leftBack =   Range.clip( drivey  + drivex + turn, -1, 1 );
 
-        fl.setPower( leftFront );
-        fr.setPower( rightFront );
-        bl.setPower( leftBack );
-        br.setPower( rightBack );
+        Robot.frontLeft.setPower( leftFront );
+        Robot.frontRight.setPower( rightFront );
+        Robot.backLeft.setPower( leftBack );
+        Robot.backRight.setPower( rightBack );
 
+        telemetry.addData( "drivey", drivey );
+        telemetry.addData( "drivex", drivex );
         telemetry.addData( "Turn", turn );
     }
     @Override
     public void init(){
-        fl = hardwareMap.dcMotor.get( "fl" );
-        fr = hardwareMap.dcMotor.get( "fr" );
-        bl = hardwareMap.dcMotor.get( "bl" );
-        br = hardwareMap.dcMotor.get( "br" );
+        Robot.init( hardwareMap );
     }
 }
